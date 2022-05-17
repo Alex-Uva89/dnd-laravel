afc1c9ea5de2d4a4ea3c172024d7c25775e91980
LaSe28 <mattia.las28@gmail.com>
2022-05-17 17:55:28 +0200

    adjust

--- resources/views/monsters/show.blade.php
+++ resources/views/monsters/show.blade.php
@@ -1,24 +1,24 @@
 @extends('template.base')
-@section('title')
+@section('title', 'Monster')
 
 @section('content')
 
 <h3>{{$monster->name}}</h3>
-<div>{{ $monster->index }} </div>
-<div>{{ $monster->type }} </div>
-<div>{{ $monster->alignment }} </div>
-<div>{{ $monster->armor_class }} </div>
-<div>{{ $monster->hit_points }} </div>
-<div>{{ $monster->size }} </div>
-<div>{{ $monster->strength }} </div>
-<div>{{ $monster->dexterity }} </div>
-<div>{{ $monster->constitution }} </div>
-<div>{{ $monster->intelligence }} </div>
-<div>{{ $monster->wisdom }} </div>
-<div>{{ $monster->charisma }} </div>
-<div>{{ $monster->xp }} </div>
-<div>{{ $monster->challenge_rating }} </div>
-<div>{{ $monster->hit_dice }} </div>
-<div>{{ $monster->languages }} </div>
+<div>index:{{ $monster->index }} </div>
+<div>type:{{ $monster->type }} </div>
+<div>alignment:{{ $monster->alignment }} </div>
+<div>armor_class:{{ $monster->armor_class }} </div>
+<div>hit_points:{{ $monster->hit_points }} </div>
+<div>size:{{ $monster->size }} </div>
+<div>strength:{{ $monster->strength }} </div>
+<div>dexterity:{{ $monster->dexterity }} </div>
+<div>constitution:{{ $monster->constitution }} </div>
+<div>intelligence:{{ $monster->intelligence }} </div>
+<div>wisdom:{{ $monster->wisdom }} </div>
+<div>charisma:{{ $monster->charisma }} </div>
+<div>xp:{{ $monster->xp }} </div>
+<div>challenge_rating:{{ $monster->challenge_rating }} </div>
+<div>hit_dice:{{ $monster->hit_dice }} </div>
+<div>languages:{{ $monster->languages }} </div>
 
 @endsection
