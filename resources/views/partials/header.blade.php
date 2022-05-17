f23957428a97def8b31135392ba6ef4ce920cf71
Martino888 <maxmartino33@gmail.com>
2022-05-17 17:34:15 +0200

    exchange route

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('monster.index') }}">Home</a>
+            <a class="navbar-brand" href="{{ route('monsters.index') }}">Home</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('monster.create')}}">Aggiungi</a>
+                    <a class="nav-link" href="{{route('monsters.create')}}">Aggiungi</a>
                 </li>
 
                 </li>
