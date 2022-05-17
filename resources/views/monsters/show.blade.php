a3a787dbe11e624be59ea3a521503f87da5e4ba8
LaSe28 <mattia.las28@gmail.com>
2022-05-17 18:26:18 +0200

    add container

--- resources/views/monsters/show.blade.php
+++ resources/views/monsters/show.blade.php
@@ -3,22 +3,24 @@
 
 @section('content')
 
-<h3>{{$monster->name}}</h3>
-<div>Index: {{ $monster->index }} </div>
-<div>Type: {{ $monster->type }} </div>
-<div>Alignment: {{ $monster->alignment }} </div>
-<div>Armor_class: {{ $monster->armor_class }} </div>
-<div>Hit_points: {{ $monster->hit_points }} </div>
-<div>Size: {{ $monster->size }} </div>
-<div>Strength: {{ $monster->strength }} </div>
-<div>Dexterity: {{ $monster->dexterity }} </div>
-<div>Constitution: {{ $monster->constitution }} </div>
-<div>Intelligence: {{ $monster->intelligence }} </div>
-<div>Wisdom: {{ $monster->wisdom }} </div>
-<div>Charisma: {{ $monster->charisma }} </div>
-<div>Xp: {{ $monster->xp }} </div>
-<div>Challenge_rating: {{ $monster->challenge_rating }} </div>
-<div>Hit_dice: {{ $monster->hit_dice }} </div>
-<div>Languages: {{ $monster->languages }} </div>
+<div class="container">
+    <h3>{{$monster->name}}</h3>
+    <div>Index: {{ $monster->index }} </div>
+    <div>Type: {{ $monster->type }} </div>
+    <div>Alignment: {{ $monster->alignment }} </div>
+    <div>Armor_class: {{ $monster->armor_class }} </div>
+    <div>Hit_points: {{ $monster->hit_points }} </div>
+    <div>Size: {{ $monster->size }} </div>
+    <div>Strength: {{ $monster->strength }} </div>
+    <div>Dexterity: {{ $monster->dexterity }} </div>
+    <div>Constitution: {{ $monster->constitution }} </div>
+    <div>Intelligence: {{ $monster->intelligence }} </div>
+    <div>Wisdom: {{ $monster->wisdom }} </div>
+    <div>Charisma: {{ $monster->charisma }} </div>
+    <div>Xp: {{ $monster->xp }} </div>
+    <div>Challenge_rating: {{ $monster->challenge_rating }} </div>
+    <div>Hit_dice: {{ $monster->hit_dice }} </div>
+    <div>Languages: {{ $monster->languages }} </div>
+</div>
 
 @endsection
