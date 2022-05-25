d8abafcae6623592fd955c19a0236d08678301d4
ALESSANDROUVA <a.uva1988@gmail.com>
2022-05-25 17:15:55 +0200

    push

--- resources/views/monsters/show.blade.php
+++ resources/views/monsters/show.blade.php
@@ -3,22 +3,33 @@
 
 @section('content')
 
-<h3>{{$monster->name}}</h3>
-<div>Index: {{ $monster->index }} </div>
-<div>Type: {{ $monster->type }} </div>
-<div>Alignment: {{ $monster->alignment }} </div>
-<div>Armor_class: {{ $monster->armor_class }} </div>
-<div>Hit_points: {{ $monster->hit_points }} </div>
-<div>Size: {{ $monster->size }} </div>
-<div>Strength: {{ $monster->strength }} </div>
-<div>Dexterity: {{ $monster->dexterity }} </div>
-<div>Constitution: {{ $monster->constitution }} </div>
-<div>Intelligence: {{ $monster->intelligence }} </div>
-<div>Wisdom: {{ $monster->wisdom }} </div>
-<div>Charisma: {{ $monster->charisma }} </div>
-<div>Xp: {{ $monster->xp }} </div>
-<div>Challenge_rating: {{ $monster->challenge_rating }} </div>
-<div>Hit_dice: {{ $monster->hit_dice }} </div>
-<div>Languages: {{ $monster->languages }} </div>
+<div class="container">
+    <div class="card m-4">
+        <h3 class="border border-warning p-2  text-center">{{$monster->name}}</h3>
+        <div class="d-flex justify-content-around">
+            <div class="p-3">
+                <div>Index: {{ $monster->index }} </div>
+                <div>Type: {{ $monster->type }} </div>
+                <div>Alignment: {{ $monster->alignment }} </div>
+                <div>Armor_class: {{ $monster->armor_class }} </div>
+                <div>Hit_points: {{ $monster->hit_points }} </div>
+                <div>Size: {{ $monster->size }} </div>
+                <div>Strength: {{ $monster->strength }} </div>
+                <div>Dexterity: {{ $monster->dexterity }} </div>
+            </div>
+            <div class="p-3">
+                <div>Constitution: {{ $monster->constitution }} </div>
+                <div>Intelligence: {{ $monster->intelligence }} </div>
+                <div>Wisdom: {{ $monster->wisdom }} </div>
+                <div>Charisma: {{ $monster->charisma }} </div>
+                <div>Xp: {{ $monster->xp }} </div>
+                <div>Challenge_rating: {{ $monster->challenge_rating }} </div>
+                <div>Hit_dice: {{ $monster->hit_dice }} </div>
+                <div>Languages: {{ $monster->languages }} </div>
+            </div>
+        </div>
+        <a class="btn btn-white" href="{{ url()->previous() }}">Indietro</a>
+
+    </div>
 
 @endsection
