448553c0f56cb100e8f8bab6062ff7e4c2586ab1
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-25 17:19:00 +0200

    ckaesjbfeqkabfa

--- resources/views/monsters/show.blade.php
+++ resources/views/monsters/show.blade.php
@@ -3,6 +3,17 @@
 
 @section('content')
 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eius iusto id asperiores totam commodi earum recusandae inventore beatae quidem, quaerat reprehenderit, dolore architecto unde quo ratione esse, neque ipsum.
+Error cumque, ut facilis libero praesentium voluptates doloremque voluptatem expedita, a nam est cum officia, odio pariatur necessitatibus. Perferendis quos quo molestias enim totam ad. Perspiciatis aperiam nemo blanditiis. Corporis.
+Dolorem eum adipisci optio molestiae aliquid aspernatur, reprehenderit facere amet eos illo! Ea accusamus animi ipsa culpa qui dolor vel eaque necessitatibus error obcaecati, magnam consequuntur iure quisquam enim veritatis?
+Debitis nisi maiores repellat sunt doloremque cupiditate similique, molestias reiciendis ex tenetur ratione, a nobis fugit corporis voluptas esse expedita magni quam et dolorum? Minima assumenda amet asperiores a fugiat?
+Minus dicta libero numquam ratione culpa iste reiciendis eum eos iure odio vitae excepturi fugit, aliquam impedit voluptate natus porro corrupti ea dolor magni sapiente. Optio nam itaque dolorum pariatur.
+A obcaecati molestiae, delectus harum commodi, labore nisi sequi asperiores sit placeat dignissimos laboriosam minus eum ratione accusantium eveniet expedita alias nam ad fugiat error? Optio eaque natus quas soluta?
+Velit in maiores soluta a beatae, aliquid exercitationem iste perferendis delectus magnam porro officia earum mollitia omnis ipsum tenetur vel veniam eaque qui. Necessitatibus qui harum ratione voluptatem, excepturi ullam?
+Ad incidunt sint natus blanditiis veniam earum nisi est. Unde dignissimos itaque blanditiis autem dolores dicta id ad amet! Fugit ut consectetur aut veritatis eaque nisi sint deleniti odit provident!
+Natus quidem minima incidunt vitae odio consequatur! Quaerat dignissimos eligendi cum pariatur, vel tempore, atque debitis quibusdam autem ea vero repellat ex voluptatibus consequatur, accusantium cupiditate ab veritatis iure in!
+Rerum illum ut doloremque quisquam quis eos. Voluptatem, pariatur. Pariatur neque voluptatem optio quisquam atque facilis fuga, aliquam dolor, explicabo sit possimus quo voluptatum sed sint consectetur molestiae exercitationem nesciunt?</p>
+
 <h3>{{$monster->name}}</h3>
 <div>Index: {{ $monster->index }} </div>
 <div>Type: {{ $monster->type }} </div>
@@ -20,5 +31,7 @@
 <div>Challenge_rating: {{ $monster->challenge_rating }} </div>
 <div>Hit_dice: {{ $monster->hit_dice }} </div>
 <div>Languages: {{ $monster->languages }} </div>
+
+<div>DAJE TUTTA</div>
 
 @endsection
