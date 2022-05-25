07da538e0af994070e51e9c1d39a3eed3f6d3079
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-25 17:50:34 +0200

    monsterProficiensie --all created

---
+++ app/Http/Controllers/MonsterProficiensieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MonsterProficiensie;
+use Illuminate\Http\Request;
+
+class MonsterProficiensieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MonsterProficiensie  $monsterProficiensie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MonsterProficiensie $monsterProficiensie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MonsterProficiensie  $monsterProficiensie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MonsterProficiensie $monsterProficiensie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MonsterProficiensie  $monsterProficiensie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MonsterProficiensie $monsterProficiensie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MonsterProficiensie  $monsterProficiensie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MonsterProficiensie $monsterProficiensie)
+    {
+        //
+    }
+}
